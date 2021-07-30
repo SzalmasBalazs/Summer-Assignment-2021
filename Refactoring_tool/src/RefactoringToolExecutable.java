59f01d30ee3c9e8a36c24b223779b061bb8214d1
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-07-30 15:53:28 +0200

    Added comments, relative directory pathing

---
+++ Refactoring_tool/src/RefactoringToolExecutable.java
@@ -0,0 +1,38 @@
+/*
+ * DirReaderExec.java
+ * 
+ * Version 0.1.2
+ *
+ * 2021.07.30
+ * 
+ */
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import directoryList.DirectoryReader;
+
+
+public class RefactoringToolExecutable {
+    public static void main(String[] args) throws Exception {
+
+        /*Relative directory pathing.
+        First, the currentDirectory variable gets the path of the current directory,
+        thanks to this, the directory path needs not to be manually changed.
+        */
+        Path currentDirectory = Paths.get("/src");
+
+        /*The newPath variable contais the path to the folder
+          with the test files for now. */ 
+        Path newPath = Paths.get("/src/test_files");
+
+        /*relativize makes a path form the current directory to the new path.*/
+        Path currentDirToPath = currentDirectory.relativize(newPath);
+
+        List<Path> paths = DirectoryReader.walk(currentDirToPath.toAbsolutePath());
+        DirectoryReader.listDirectoryContents(paths);
+
+        
+    }
+
+}
