ffade34f515574471ff93f4e4d6954dde3a685d2
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-08-10 17:38:47 +0200

    Added extra test variables

--- Refactoring_tool/src/test_files/test.java
+++ Refactoring_tool/src/test_files/test.java
@@ -2,6 +2,9 @@ package test_files;
 
 public class test {
     public static void main(String[] args) throws Exception {
+
+        int i = 0;
         System.out.println("Hello World");
+        System.out.println(i);
     }
 }
