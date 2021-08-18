3da67cb658aee14deea3ea677cfb3a0794c949dd
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-08-18 15:51:37 +0200

    Updated with new methods, list in description.
    -Added walkTree method: A method that lets us traverse the tree, currently looking and listing all methods of the test file.
    -Moved the printers into their own methods.
    -Added JavaDoc for methods.

--- Maven/com.szalmas.maven.eclipse/src/main/java/com/szalmas/com/szalmas/maven/eclipse/LibraryHandler/methodParser.java
+++ Maven/com.szalmas.maven.eclipse/src/main/java/com/szalmas/com/szalmas/maven/eclipse/LibraryHandler/methodParser.java
@@ -2,28 +2,42 @@ package com.szalmas.com.szalmas.maven.eclipse.LibraryHandler;
 
 
 	import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.StaticJavaParser;
+	import com.github.javaparser.ast.body.MethodDeclaration;
+	import com.github.javaparser.StaticJavaParser;
 	import com.github.javaparser.printer.DotPrinter;
 	import com.github.javaparser.printer.YamlPrinter;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Path;
+	 
+	import java.io.FileWriter;
+	import java.io.IOException;
+	import java.io.PrintWriter;
+	import java.nio.file.Path;
 
 
 public class methodParser {
 
 	
-	@SuppressWarnings("unchecked")
+	/**
+     *	Uses JavaParser to generate AST(s) from the files on the designated path.
+     * @param path
+     */
 	public static void MethodCallPrinter (Path path) throws IOException {
 	
 		CompilationUnit cu = StaticJavaParser.parse(path);
 		
-		/*YamlPrinter printer = new YamlPrinter(true);
-		System.out.println(printer.output(cu));
-		*/
+		callYalmPrinter(cu);
+		callDotPrinter(cu);
+		walkTree(cu);
+		
+	//	new myMethodVisitor().visit(cu, null);
+		
+	}
+	
+	/**
+     *	DotPrinter lets put out a dot file that contains the nodes on an AST, after this using GraphViz
+     *	we are able to make a very nice graph of our AST(s).
+     * @param cu
+     */
+	private static void callDotPrinter(CompilationUnit cu) throws IOException {
 		
 		DotPrinter printer = new DotPrinter(true);
 		try(FileWriter fileWriter = new FileWriter("ast.dot");
@@ -31,17 +45,26 @@ public class methodParser {
 					    printWriter.print(printer.output(cu));
 		}
 		
-		walkItLikeITalkIt(cu);
-	//	new myMethodVisitor().visit(cu, null);
-		
-		
 	}
-	public static void walkItLikeITalkIt (CompilationUnit cu) {
+
+	/**
+     *	JavaParser's printer library, YalmPrinter lets you print out the AST in your console.
+     * @param cu
+     */
+	private static void callYalmPrinter(CompilationUnit cu) {
+		YamlPrinter printer = new YamlPrinter(true);
+		System.out.println(printer.output(cu));
+	}
+
+	/**
+     * walks the tree, listing all the nodes with method declarations.
+     * @param cu
+     */
+	public static void walkTree (CompilationUnit cu) {
 		
-		Node.BreadthFirstIterator iterator = new Node.BreadthFirstIterator(cu);
-        while (iterator.hasNext()) {
-            System.out.println("* " + iterator.next());
+		cu.findAll(MethodDeclaration.class).forEach(node -> System.out.println("* " + node));
+		
         }	
 		
 	}
-}
+
