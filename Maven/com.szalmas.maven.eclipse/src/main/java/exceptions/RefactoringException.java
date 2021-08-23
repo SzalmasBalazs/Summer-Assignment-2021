2f127d09637ce025b9eddef3f14cb41ad2bdefce
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-08-23 17:15:55 +0200

    Added custom expression

---
+++ Maven/com.szalmas.maven.eclipse/src/main/java/exceptions/RefactoringException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class RefactoringException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	  public RefactoringException(String message) {
+	        super(message);
+	  }
+
+}
